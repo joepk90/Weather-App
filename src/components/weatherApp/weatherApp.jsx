fd16387c051888dd5941333a55808e8bdd5d993c
Joe <jparkkennaby@gmail.com>
2020-10-24 17:36:44 +0100

    setup weatherApp to request openWeatherMap weather data. update weatherAPP state with current weather data

--- src/components/weatherApp/weatherApp.jsx
+++ src/components/weatherApp/weatherApp.jsx
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 
 import Header from '~components/header/header';
 import ForecastList from '~components/forecastList/forecastList';
+import { getWeather } from '~services/openWeatherMapServices';
 
 import "~components/weatherApp/weatherApp.scss";
 
-
 class WeatherApp extends Component {
-    state = {}
+    state = {
+        currentWeatherData: {}
+    }
+
+
+    async componentDidMount() {
+
+        let currentWeatherData = await getWeather();
+
+        this.setState(
+            {
+                currentWeatherData
+            }
+        );
+
+
+    }
+
+
     render() {
+
+        const { currentWeatherData } = this.state;
+
         return (
             <React.Fragment>
 
-                <Header />
+                <Header weather={currentWeatherData} />
 
                 <main>
                     <ForecastList />
