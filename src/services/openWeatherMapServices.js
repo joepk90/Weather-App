03bbdf7eec97266d66605db0e23e47b6d0092727
Joe <jparkkennaby@gmail.com>
2020-10-24 16:44:05 +0100

    setup httpServices using axios

---
+++ src/services/openWeatherMapServices.js
@@ -0,0 +1,24 @@
+import axios from '~services/httpsService';
+
+const APPID = process.env.REACT_APP_BASE_URL_OPEN_WEATHER_MAP_API_KEY;
+
+function axiosGetData(endPoint) {
+
+    const axiosParams = {
+        params: {
+            q: 'London,UK',
+            APPID: APPID
+        }
+    }
+
+    return axios.get(endPoint, axiosParams);
+}
+
+export function getForecast() {
+    return axiosGetData('forecast');
+}
+
+
+export function getWeather() {
+    return axiosGetData('weather');
+}
